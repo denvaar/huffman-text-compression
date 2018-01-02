bd2457d3b5c65522567f38b2a6b506cf330e37cf
Denver Smith <denverpsmith@gmail.com>
2018-01-01 20:49:52 -0700

    first iteration of saving to file

--- lib/file.ex
+++ lib/file.ex
@@ -1,16 +1,21 @@
+require IEx
 defmodule HuffmanFile do
   @moduledoc """
   File function
   """
 
-  def ensure_binary(bitz) when is_binary(bitz), do: bitz
-  def ensure_binary(bitz), do: ensure_binary(<< bitz::bitstring, <<0::1>> >>)
+  defp ensure_binary(bits) when is_binary(bits), do: bits
+  defp ensure_binary(bits), do: ensure_binary(<< bits::bitstring, <<0::1>> >>)
+
+  defp serialize_huffman_tree(tree), do: :erlang.term_to_binary(tree)
+
+  defp deserialize_huffman_tree(bytes), do: :erlang.binary_to_term(bytes)
 
   def compress(path, text) do
-    {bits, _tree} = Huffman.compress(text)
-    bytes = ensure_binary(bits)
+    {bits, tree} = Huffman.compress(text)
+    serialized_tree = serialize_huffman_tree(tree)
 
-    case File.write(path, bytes) do
+    case File.write(path, to_string(byte_size(serialized_tree)) <> "|" <> serialized_tree <> ensure_binary(bits)) do
       :ok ->
         "Compression complete!"
       {:error, reason} ->
@@ -18,7 +23,26 @@ defmodule HuffmanFile do
     end
   end
 
+  defp chomp_size(<<digit::size(8)>> <> rest) do
+    case <<digit>> do
+      "|" ->
+        {"", rest}
+      _anything_else ->
+        {size, remaining_bytes} = chomp_size(rest)
+        {<<digit>> <> size, remaining_bytes}
+    end
+  end
+
   def decompress(path) do
-    # TODO
+    case File.read(path) do
+      {:ok, encoded_content} ->
+        {size_of_tree, remaining_content} = chomp_size(encoded_content)
+        sz = String.to_integer(size_of_tree)
+        << tree_bytes::binary-size(sz), rest::binary >> = remaining_content
+        huffman_tree = deserialize_huffman_tree(tree_bytes)
+        Huffman.decompress(rest, huffman_tree)
+      {:error, reason} ->
+        "Unable to decompress file: #{reason}"
+    end
   end
 end
--- lib/huffman.ex
+++ lib/huffman.ex
@@ -13,50 +13,50 @@ defmodule Huffman do
     |> Enum.chunk_by(fn arg -> arg end)
     |> Enum.sort_by(&length/1)
     |> Enum.reduce([], fn(chunk, acc) ->
-      [%{type: :leaf, character: List.first(chunk), freq: length(chunk)}|acc]
+      [%{t: :l, c: List.first(chunk), f: length(chunk)}|acc]
     end)
     |> Enum.reverse
   end
 
   defp sorted_nodes(nodes) do
     nodes
-    |> Enum.sort_by(fn obj -> obj.freq end)
+    |> Enum.sort_by(fn obj -> obj.f end)
   end
 
   def build_huffman_tree([]), do: nil
   def build_huffman_tree([tree]), do: tree
   def build_huffman_tree([left, right]) do
-    %{type: :node, character: nil, freq: left.freq + right.freq, left: left, right: right}
+    %{t: :n, c: nil, f: left.f + right.f, l: left, r: right}
   end
   def build_huffman_tree(nodes) do
     [first_smallest, second_smallest|rest] = sorted_nodes(nodes) # TODO this is not very optimal
 
     parent = %{
-      type: :node,
-      character: nil,
-      freq: first_smallest.freq + second_smallest.freq,
-      left: first_smallest,
-      right: second_smallest
+      t: :n,
+      c: nil,
+      f: first_smallest.f + second_smallest.f,
+      l: first_smallest,
+      r: second_smallest
     }
 
     build_huffman_tree([parent|rest])
   end
 
-  defp decode_helper(<< <<0::1>>, rest::bitstring >>, %{type: :node, freq: _, character: _, left: left, right: _}) do
+  defp decode_helper(<< <<0::1>>, rest::bitstring >>, %{t: :n, f: _, c: _, l: left, r: _}) do
     decode_helper(rest, left)
   end
-  defp decode_helper(<< <<1::1>>, rest::bitstring >>, %{type: :node, freq: _, character: _, left: _, right: right}) do
+  defp decode_helper(<< <<1::1>>, rest::bitstring >>, %{t: :n, f: _, c: _, l: _, r: right}) do
     decode_helper(rest, right)
   end
   defp decode_helper(<<>>, tree) do
-    {<<>>, tree.character}
+    {<<>>, tree.c}
   end
-  defp decode_helper(encoded_bits, %{type: :leaf, character: character, freq: _freq}) do
+  defp decode_helper(encoded_bits, %{t: :l, c: character, f: _freq}) do
     {encoded_bits, character}
   end
 
   defp decode(<<>>, _tree, decoded_text), do: decoded_text
-  defp decode(<< <<_::1>>, rest::bitstring >>, %{type: :leaf, character: c, freq: _freq} = leaf, decoded_text), do: decode(rest, leaf, decoded_text <> c)
+  defp decode(<< <<_::1>>, rest::bitstring >>, %{t: :l, c: c, f: _freq} = leaf, decoded_text), do: decode(rest, leaf, decoded_text <> c)
   defp decode(encoded_bits, tree, decoded_text) do
     case decode_helper(encoded_bits, tree) do
       {<<>>, decoded_character} ->
@@ -70,19 +70,19 @@ defmodule Huffman do
     text
     |> String.graphemes
     |> Enum.reduce(<<>>, fn(letter, bits) ->
-      << bits::bitstring, Enum.find(huffman_mapping, fn obj -> obj.character == letter end).encoding::bitstring >>
+      << bits::bitstring, Enum.find(huffman_mapping, fn obj -> obj.c == letter end).e::bitstring >>
     end)
   end
 
-  defp decorate_tree(%{type: :node, left: left_child, right: right_child, character: _, freq: _}, bits) do
+  defp decorate_tree(%{t: :n, l: left_child, r: right_child, c: _, f: _}, bits) do
     [decorate_tree(left_child, << bits::bitstring, <<0::1>> >>),
      decorate_tree(right_child, << bits::bitstring, <<1::1>> >>)]
   end
-  defp decorate_tree(%{type: :leaf, character: character, freq: _}, <<>>) do
-    [%{character: character, encoding: <<1::1>>}]
+  defp decorate_tree(%{t: :l, c: character, f: _}, <<>>) do
+    [%{c: character, e: <<1::1>>}]
   end
-  defp decorate_tree(%{type: :leaf, character: character, freq: _}, bits) do
-    [%{character: character, encoding: bits}]
+  defp decorate_tree(%{t: :l, c: character, f: _}, bits) do
+    [%{c: character, e: bits}]
   end
   defp decorate_tree(nil, <<>>), do: []
 
@@ -94,15 +94,24 @@ defmodule Huffman do
   @doc """
   Compress text using Huffman Coding algorithm
 
+  For the Huffman Tree I used the smallest possible key names in order
+  to save as many bits as possible when doing `:erlang.term_to_binary`
+
+  c --> character
+  f --> frequency
+  l --> left
+  r --> right
+  t --> type (:node or :leaf)
+
   ## Examples
 
       iex> Huffman.compress("aaabbbccc")
       {<<1, 47::size(7)>>,
-       %{character: nil, freq: 9,
-         left: %{character: nil, freq: 6,
-           left: %{character: "a", freq: 3, type: :leaf},
-           right: %{character: "b", freq: 3, type: :leaf}, type: :node},
-         right: %{character: "c", freq: 3, type: :leaf}, type: :node}}
+       %{c: nil, f: 9,
+         l: %{c: nil, f: 6,
+           l: %{c: "a", f: 3, t: :l},
+           r: %{c: "b", f: 3, t: :l}, t: :n},
+         r: %{c: "c", f: 3, t: :l}, t: :n}}
 
   """
   def compress(text) do
@@ -121,7 +130,7 @@ defmodule Huffman do
 
   ## Examples
 
-      iex> Huffman.decompress(<<13::size(6)>>, %{character: nil, freq: 4, left: %{character: nil, freq: 2, left: %{character: "b", freq: 1, type: :leaf}, right: %{character: "t", freq: 1, type: :leaf}, type: :node}, right: %{character: "o", freq: 2, type: :leaf}, type: :node})
+      iex> Huffman.decompress(<<13::size(6)>>, %{c: nil, f: 4, l: %{c: nil, f: 2, l: %{c: "b", f: 1, t: :l}, r: %{c: "t", f: 1, t: :l}, t: :n}, r: %{c: "o", f: 2, t: :l}, t: :n})
       "boot"
 
   """
