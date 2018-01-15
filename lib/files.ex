6ea044e900fca3234501672f7f1881f6bd92ea32
Denver Smith <denverpsmith@gmail.com>
2018-01-15 09:50:22 -0700

    All tests passing :sun:

--- lib/files.ex
+++ lib/files.ex
@@ -1,4 +1,3 @@
-require IEx
 defmodule Huffman.Files do
   @moduledoc """
   Functions for reading and writing files.
@@ -38,7 +37,7 @@ defmodule Huffman.Files do
     |> String.pad_leading(max_width, "0")
   end
 
-  def serialize(mapping) do
+  defp serialize(mapping) do
     max_bytes_to_read_width =
       mapping
       |> Map.values()
@@ -63,7 +62,7 @@ defmodule Huffman.Files do
     mapping
     |> Map.to_list()
     |> Enum.reduce(initial_text, fn({width, mappings}, acc) ->
-      symbols = Map.values(mappings)
+      symbols = Enum.sort(Map.values(mappings))
 
       acc \
       <> serialize_number_of_bytes_to_read_next(symbols, max_bytes_to_read_width) \
@@ -72,26 +71,21 @@ defmodule Huffman.Files do
     end)
   end
 
+  defp deserialize(<<48, 32, remaining::bitstring>>, _to_read_width, _group_width, m),
+    do: {remaining, Huffman.Decoder.prep_map_for_decoding(Huffman.Encoder.assign_ascending_bits(Enum.reverse(m), 0))}
 
-  def deserialize(<<48, 32, remaining::bitstring>>, _to_read_width, _group_width, m) do
-    {remaining, Huffman.Decoder.prep_map_for_decoding(Huffman.Encoder.assign_ascending_bits(Enum.reverse(m), 0))}
-  end
-
-  def deserialize(header, to_read_width, group_width, m) do
-    <<first::binary-size(to_read_width), second::binary-size(group_width), remaining::bitstring>> = header
+  defp deserialize(header, to_read_width, group_width, m) do
+    <<first::binary-size(to_read_width), second::binary-size(group_width), remaining_header::bitstring>> = header
     number_of_bytes_to_read = String.to_integer(first)
     bit_length = String.to_integer(second)
-    <<bytes_for_group::binary-size(number_of_bytes_to_read), y::bitstring>> = remaining
+    <<bytes_for_group::binary-size(number_of_bytes_to_read), remaining::bitstring>> = remaining_header
 
-    wow =
+    group_mapping =
       bytes_for_group
-      |> IO.inspect()
       |> String.graphemes()
       |> Enum.map(fn symb -> {symb, bit_length} end)
 
-    IO.inspect(wow)
-
-    deserialize(y, to_read_width, group_width, [wow | m])
+    deserialize(remaining, to_read_width, group_width, [group_mapping | m])
   end
 
   defp ensure_binary(bits) when is_binary(bits), do: bits
@@ -108,8 +102,6 @@ defmodule Huffman.Files do
     number_of_junk_bits = to_string(bit_size(ensure_binary(compressed_bits)) - bit_size(compressed_bits))
     final_data = << number_of_junk_bits::binary, serialize(mapping)::binary, <<48, 32>>, ensure_binary(compressed_bits)::binary >>
 
-    IO.inspect(serialize(mapping))
-
     case File.write(path, final_data) do
       :ok -> "Data written to -> #{path}"
       {:error, reason} -> "Unable to write compressed data: #{reason}"
@@ -119,7 +111,7 @@ defmodule Huffman.Files do
   defp write_file(uncompressed_text, path) do
     case File.write(path, uncompressed_text) do
       :ok -> "File decompression complete #{path}"
-      {:error, reason} -> "Unable to decompress data: #{path}"
+      {:error, reason} -> "Unable to decompress data: #{path} #{reason}"
     end
   end
 
@@ -142,13 +134,19 @@ defmodule Huffman.Files do
         group_size_width = width_from_classifier(second)
 
         bit_diff = bit_size(compressed_data) - String.to_integer(number_of_leftover_bits)
-        <<cd::size(bit_diff), _junk_bits::bitstring>> = compressed_data
+        <<compressed_data_without_extra_bits::size(bit_diff), _junk_bits::bitstring>> = compressed_data
 
-        {encoded_data, mapping} = deserialize(<<cd::size(bit_diff)>>, bytes_to_read_width, group_size_width, [])
+        {encoded_data, mapping} = deserialize(
+          <<compressed_data_without_extra_bits::size(bit_diff)>>,
+          bytes_to_read_width,
+          group_size_width,
+          []
+        )
+
         Huffman.decompress(encoded_data, mapping)
         |> write_file(out_file_path)
 
-      {:error, reason} -> "Unable to read input file"
+      {:error, reason} -> "Unable to read input file: #{reason}"
     end
   end
 end
